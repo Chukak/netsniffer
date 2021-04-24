65ec2dde206ea25ff153b4a3f66ea8d82ebc8fa5
Sergey Fedorov <chukak12@gmail.com>
2021-04-24 19:27:15 +0300

    Parsing command-line arguments
    Print help

---
+++ src/cmdargs.c
@@ -0,0 +1,70 @@
+#include "cmdargs.h"
+
+#include "utils.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+ParseArgsReturnCode_t ParseCommandLineArgs(int argc, char** argv, CmdArgs_t* args, char** error)
+{
+  if (args == NULL) {
+    FormatStringBuffer(error, "The pointer to CmdArgs_t == NULL.");
+    return CmdArgs_ERROR;
+  }
+
+  args->Address = "";
+  args->Protocol = Protocol_ANY;
+
+  for (int i = 1; i < argc; ++i) {
+    char* arg = argv[i];
+    if (strcmp(arg, "-protocol") == 0) {
+      if (i + 1 >= argc) {
+        FormatStringBuffer(error, "No the protocol value after '-protocol' option.");
+        return CmdArgs_ERROR;
+      }
+      args->Protocol = GetProtocolFromString(argv[++i]);
+    } else if (strcmp(arg, "-help") == 0) {
+      return CmdArgs_PRINT_HELP;
+    } else {
+      char *ip = NULL, *iface = NULL;
+      int port;
+      int rc = ParseAddressString(arg, &iface, &ip, &port, error);
+      free(ip);
+      free(iface);
+      if (rc != 0) {
+        return CmdArgs_ERROR;
+      }
+      args->Address = arg;
+    }
+  }
+
+  if (strlen(args->Address) == 0) {
+    FormatStringBuffer(error, "No address specified.");
+    return CmdArgs_ERROR;
+  }
+  return CmdArgs_SUCCESS;
+}
+
+void PrintHelp()
+{
+  const char* message = "Usage: " EXE_BINARY_NAME " OPTIONS... "
+#ifdef __linux__
+                        "INTERFACE:IP:PORT"
+#elif _WIN32
+                        "IP:PORT"
+#endif
+                        " \n"
+                        "Sniffs network traffic at the specified address. \n"
+                        "Arguments: \n"
+                        "\t-protocol [ICMP, TCP, UDP]\t\tNetwork protocol. \n"
+                        "\n"
+                        "Example: \n"
+#ifdef __linux__
+                        "\t" EXE_BINARY_NAME " eth0:127.0.0.1:8000 \n"
+#elif _WIN32
+                        "\t" EXE_BINARY_NAME ".exe 127.0.0.1:8000 \n"
+#endif
+                        "\n";
+  printf("%s", message);
+}
---
+++ src/cmdargs.h
@@ -0,0 +1,40 @@
+#ifndef __CMDARGS_H
+#define __CMDARGS_H
+
+#include "structures.h"
+
+/**
+ * @brief ParseArgsReturnCode_t
+ * Result codes of parsing command line arguments.
+ */
+typedef enum
+{
+  CmdArgs_ERROR = -1,
+  CmdArgs_SUCCESS = 0,
+  CmdArgs_PRINT_HELP = 1
+} ParseArgsReturnCode_t;
+/**
+ * @brief CmdArgs_t
+ * Stores arguments from command line.
+ */
+typedef struct
+{
+  const char* Address;
+  Protocol_t Protocol;
+} CmdArgs_t;
+/**
+ * @brief ParseCommandLineArgs
+ * Parses command line arguments. Stores these arguments to the passed CmdArgs_t object.
+ * @param argc Arguments size
+ * @param argv Arguments array
+ * @param error Error message (if occurred)
+ * @return CmdArgs_ERROR if an error occurred, otherwise CmdArgs_SUCCESS or other codes.
+ */
+ParseArgsReturnCode_t ParseCommandLineArgs(int argc, char** argv, CmdArgs_t* args, char** error);
+/**
+ * @brief PrintHelp
+ * Print the help information.
+ */
+void PrintHelp();
+
+#endif // __CMDARGS_H
